d57cfcd4a1e1cd251cf5154ac6b29942e0043ec2
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2015-02-08 00:20:56 -0800

    use soil2 for DXT1 and DXT5 compression
    An intermediate surface is only created if the source is not RGB for DXT1 and RGBA for DXT5.

--- src/TexturizerApp.cpp
+++ src/TexturizerApp.cpp
@@ -10,7 +10,9 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/Texture.h"
 
-#include "stb_dxt.h"
+extern "C" {
+    #include "image_DXT.h"
+}
 
 using namespace ci;
 using namespace ci::app;
@@ -44,26 +46,38 @@ void TexturizerApp::setup() {
         quit();
     }
 
-    // create RGBA intermediate when necessary
-    if (mSourceSurface->getChannelOrder().getCode() != SurfaceChannelOrder::RGBA) {
+    // create intermediate when necessary
+#if defined(DXT1)
+    SurfaceChannelOrder channelOrder = SurfaceChannelOrder::RGB;
+#else
+    SurfaceChannelOrder channelOrder = SurfaceChannelOrder::RGBA;
+#endif
+    if (mSourceSurface->getChannelOrder().getCode() != channelOrder.getCode()) {
         int32_t width = mSourceSurface->getWidth();
         int32_t height = mSourceSurface->getHeight();
-        size_t size = width * height * 4;
-        unsigned char* intermediate = (unsigned char*)malloc(size);
+#if defined(DXT1)
+        int32_t rowBytes = 3;
+#else
+        int32_t rowBytes = 4;
+#endif
+        size_t dataSize = width * height * rowBytes;
+        unsigned char* intermediate = (unsigned char*)malloc(dataSize);
         size_t offset = 0;
 
         Surface::Iter iter = mSourceSurface->getIter();
-        while(iter.line()) {
-            while(iter.pixel()) {
+        while (iter.line()) {
+            while (iter.pixel()) {
                 intermediate[offset++] = iter.r();
                 intermediate[offset++] = iter.g();
                 intermediate[offset++] = iter.b();
+#if !defined(DXT1)
                 intermediate[offset++] = mSourceSurface->hasAlpha() ? iter.a() : 255;
-           }
+#endif
+            }
         }
 
         try {
-            mIntermediateSurface = Surface::create(intermediate, width, height, 4, SurfaceChannelOrder::RGBA);
+            mIntermediateSurface = Surface::create(intermediate, width, height, rowBytes, channelOrder);
         } catch (...) {
             console() << "failed to create intermediate RGBA surface" << endl;
             quit();
@@ -72,143 +86,35 @@ void TexturizerApp::setup() {
 
     Surface8uRef s = mIntermediateSurface ? mIntermediateSurface : mSourceSurface;
     unsigned char* source = s->getData();
-    int32_t width = s->getWidth();
-    int32_t height = s->getHeight();
-    size_t dataSize = s->getPixelBytes() * width * height;
+    int width = s->getWidth();
+    int height = s->getHeight();
+
+    int dataSize = 0;
 #if defined(DXT1)
-    dataSize /= 8;
-    int compressionFormat = 0;
+    unsigned char* destination = convert_image_to_DXT1(source, width, height, 3, &dataSize);
 #else
-    dataSize /= 4;
-    int compressionFormat = 1;
+    unsigned char* destination = convert_image_to_DXT5(source, width, height, 4, &dataSize);
 #endif
-    unsigned char* destination = (unsigned char*)malloc(dataSize);
 
-    rygCompress(destination, source, width, height, compressionFormat);
-
-    // lifted from TextureFormatParsers.cpp parseDds
-    typedef struct { // DDCOLORKEY
-        uint32_t dw1;
-        uint32_t dw2;
-    } ddColorKey;
-
-    typedef struct  { // DDSCAPS2
-        uint32_t dwCaps1;
-        uint32_t dwCaps2;
-        uint32_t Reserved[2];
-    } ddCaps2;
-
-    typedef struct { // DDPIXELFORMAT
-        uint32_t  dwSize;
-        uint32_t  dwFlags;
-        uint32_t  dwFourCC;
-        union {
-            uint32_t  dwRGBBitCount;
-            uint32_t  dwYUVBitCount;
-            uint32_t  dwZBufferBitDepth;
-            uint32_t  dwAlphaBitDepth;
-            uint32_t  dwLuminanceBitCount;
-            uint32_t  dwBumpBitCount;
-            uint32_t  dwPrivateFormatBitCount;
-        } ;
-        union {
-            uint32_t  dwRBitMask;
-            uint32_t  dwYBitMask;
-            uint32_t  dwStencilBitDepth;
-            uint32_t  dwLuminanceBitMask;
-            uint32_t  dwBumpDuBitMask;
-            uint32_t  dwOperations;
-        } ;
-        union {
-            uint32_t  dwGBitMask;
-            uint32_t  dwUBitMask;
-            uint32_t  dwZBitMask;
-            uint32_t  dwBumpDvBitMask;
-            struct {
-                int32_t wFlipMSTypes;
-                int32_t wBltMSTypes;
-            } MultiSampleCaps;
-        };
-        union {
-            uint32_t  dwBBitMask;
-            uint32_t  dwVBitMask;
-            uint32_t  dwStencilBitMask;
-            uint32_t  dwBumpLuminanceBitMask;
-        };
-        union {
-            uint32_t  dwRGBAlphaBitMask;
-            uint32_t  dwYUVAlphaBitMask;
-            uint32_t  dwLuminanceAlphaBitMask;
-            uint32_t  dwRGBZBitMask;
-            uint32_t  dwYUVZBitMask;
-        } ;
-    } DdPixelFormat;
-
-    typedef struct DdSurface // this is lifted and adapted from DDSURFACEDESC2
-    {
-        uint32_t               dwSize;                 // size of the DDSURFACEDESC structure
-        uint32_t               dwFlags;                // determines what fields are valid
-        uint32_t               dwHeight;               // height of surface to be created
-        uint32_t               dwWidth;                // width of input surface
-        union
-        {
-            int32_t            lPitch;                 // distance to start of next line (return value only)
-            uint32_t           dwLinearSize;           // Formless late-allocated optimized surface size
-        };
-        union
-        {
-            uint32_t           dwBackBufferCount;      // number of back buffers requested
-            uint32_t           dwDepth;                // the depth if this is a volume texture
-        };
-        union
-        {
-            uint32_t			dwMipMapCount;          // number of mip-map levels requestde
-                                                    // dwZBufferBitDepth removed, use ddpfPixelFormat one instead
-            uint32_t			dwRefreshRate;          // refresh rate (used when display mode is described)
-            uint32_t			dwSrcVBHandle;          // The source used in VB::Optimize
-        };
-        uint32_t				dwAlphaBitDepth;        // depth of alpha buffer requested
-        uint32_t				dwReserved;             // reserved
-        uint32_t				lpSurface;              // pointer to the associated surface memory
-        union
-        {
-            ddColorKey			ddckCKDestOverlay;      // color key for destination overlay use
-            uint32_t			dwEmptyFaceColor;       // Physical color for empty cubemap faces
-        };
-        ddColorKey          ddckCKDestBlt;          // color key for destination blt use
-        ddColorKey          ddckCKSrcOverlay;       // color key for source overlay use
-        ddColorKey          ddckCKSrcBlt;           // color key for source blt use
-        union
-        {
-            DdPixelFormat		ddpfPixelFormat;        // pixel format description of the surface
-            uint32_t			dwFVF;                  // vertex format description of vertex buffers
-        };
-        ddCaps2			ddsCaps;                // direct draw surface capabilities
-        uint32_t		dwTextureStage;         // stage in multitexture cascade
-    } DdSurface;
-
-    enum { FOURCC_DXT1 = 0x31545844, FOURCC_DXT3 = 0x33545844, FOURCC_DXT5 = 0x35545844, FOURCC_DX10 = 0x30315844,
-        FOURCC_ATI1 = 0x31495441, FOURCC_ATI2= 0x32495441, DDPF_FOURCC = 0x4 };
-
-    DdSurface* ddsd = (DdSurface*)calloc(4, sizeof(DdSurface));
-    ddsd->dwWidth = width;
-    ddsd->dwHeight = height;
-
-    ddsd->ddpfPixelFormat.dwFlags = DDPF_FOURCC;
+    DDS_header* header = (DDS_header*)calloc(4, sizeof(DDS_header));
+    header->dwMagic = ('D' << 0) | ('D' << 8) | ('S' << 16) | (' ' << 24);
+    header->dwSize = 124;
+    header->dwFlags = DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH | DDSD_PIXELFORMAT | DDSD_LINEARSIZE;
+    header->dwHeight = height;
+    header->dwWidth = width;
+    header->dwPitchOrLinearSize = dataSize;
+    header->sPixelFormat.dwSize = 32;
+    header->sPixelFormat.dwFlags = DDPF_FOURCC;
 #if defined(DXT1)
-    ddsd->ddpfPixelFormat.dwFourCC = FOURCC_DXT1;
+    header->sPixelFormat.dwFourCC = ('D' << 0) | ('X' << 8) | ('T' << 16) | ('1' << 24);
 #else
-    ddsd->ddpfPixelFormat.dwFourCC = FOURCC_DXT5;
+    header->sPixelFormat.dwFourCC = ('D' << 0) | ('X' << 8) | ('T' << 16) | ('5' << 24);
 #endif
-//    ddsd->ddpfPixelFormat.dwRGBBitCount =
+    header->sCaps.dwCaps1 = DDSCAPS_TEXTURE;
 
-//    ddsd->dwLinearSize = 
-//    ddsd->dwMipMapCount =
-
-    Buffer buffer(128 + dataSize);
-    memcpy((unsigned char*)buffer.getData() + 0, &"DDS ", 4);
-    memcpy((unsigned char*)buffer.getData() + 4, ddsd, 124);
-    memcpy((unsigned char*)buffer.getData() + 128, destination, dataSize);
+    Buffer buffer(sizeof(DDS_header) + dataSize);
+    memcpy((unsigned char*)buffer.getData(), header, sizeof(DDS_header));
+    memcpy((unsigned char*)buffer.getData() + sizeof(DDS_header), destination, dataSize);
     free(destination);
 
     fs::path ddsFilePath = getAssetPath("") / "out.DDS";
